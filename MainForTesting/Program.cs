94fb8ba5f96f1055725ea5d14b0f3f2737894784
JC <jcal.d.as.1.9.99+vivasgithub@gmail.com>
2020-10-16 10:18:29 +0200

    Initial commit.

---
+++ MainForTesting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuBuilder;
+
+// Demonstration of Menu's usage.
+
+namespace MainForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+
+            menu.Options.Add(new Option("Option 1", Opt1));
+            menu.Options.Add(new Option("Option 2", Opt2));
+            menu.Options.Add(new Option("Option 3", Opt2));
+
+            menu.Loop = true;
+            menu.QuitOption = true;
+
+            menu.Serve();
+        }
+
+        private static void Opt1()
+        {
+            Console.WriteLine("Option 1...");
+        }
+
+        private static void Opt2()
+        {
+            Console.WriteLine("Option 2...");
+        }
+
+        private static void Opt3()
+        {
+            Console.WriteLine("Option 3...");
+        }
+    }
+}
---
+++ Menu/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuBuilder
+{
+    public class Option
+    {
+        public string Title { set; get; }
+        public Action Callback { set; get; }
+
+        public Option(string title, Action callback)
+        {
+            Title = title;
+            Callback = callback;
+        }
+    }
+
+    public class Menu
+    {
+        public List<Option> Options { set; get; }
+        public bool Loop;
+        public bool QuitOption;
+
+        public Menu()
+        {
+            Options = new List<Option>();
+            Loop = false;
+            QuitOption = false;
+        }
+
+        private class NoOptionsException : Exception { }
+
+        public void Serve()
+        {
+            if (Options.Count == 0)
+            {
+                throw new NoOptionsException();
+            }
+
+            bool quit = false;
+            int totalOptions = Options.Count + (QuitOption ? 1 : 0);
+            int selectedOption = 0;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < Options.Count; i++)
+                {
+                    PrintOption(Options[i].Title, i == selectedOption);
+                }
+
+                if (QuitOption)
+                {
+                    PrintOption(Loop ? "Salir" : "Cancelar", selectedOption == Options.Count);
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        selectedOption = (selectedOption + 1) % totalOptions;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        selectedOption = (selectedOption - 1) % totalOptions;
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+
+                        if (selectedOption == Options.Count)
+                        {
+                            quit = true;
+                        }
+                        else
+                        {
+                            Options[selectedOption].Callback();
+
+                            ConsoleColor oldBg = Console.BackgroundColor;
+                            ConsoleColor oldFg = Console.ForegroundColor;
+
+                            Console.WriteLine();
+                            Console.BackgroundColor = oldFg;
+                            Console.ForegroundColor = oldBg;
+                            Console.Write("Pulsa ENTER para volver. ");
+                            Console.BackgroundColor = oldBg;
+                            Console.ForegroundColor = oldFg;
+
+                            while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+                            if (!Loop)
+                            {
+                                quit = true;
+                            }
+                        }
+                        break;
+                }
+            } while (!quit);
+        }
+
+        private static void PrintOption(string title, bool isSelected)
+        {
+            if (isSelected)
+            {
+                InvertConsoleColors();
+            }
+
+            Console.WriteLine("[{0}] {1}", isSelected ? "+" : " ", title);
+
+            if (isSelected)
+            {
+                InvertConsoleColors();
+            }
+        }
+
+        private static void InvertConsoleColors()
+        {
+            ConsoleColor oldBg = Console.BackgroundColor;
+            Console.BackgroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = oldBg;
+        }
+    }
+}
